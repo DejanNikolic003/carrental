4fc05bb492ff3bc1b79ca3924c04c81e54e0b595
DZSONY <knolekjhnny@gmail.com>
2024-10-16 09:56:52 +0200

    Create and migrate cars and manufacturers tables

---
+++ database/migrations/2024_10_16_074851_create_cars_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('manufacturer')->constrained()->cascadeOnDelete();
+            $table->string('model');
+            $table->date('year');
+            $table->string('color');
+            $table->integer('mileage');
+            $table->string('vin')->unique();
+            $table->enum('transmission', ['Manual', 'Automatic']);
+            $table->string('engine');
+            $table->enum('fuel_type', ['Petrol', 'Diesel', 'Electric']);
+            $table->decimal('price', 8, 2);
+            $table->enum('status', ['Available', 'Sold', 'Pending']);
+            $table->description('text');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
---
+++ database/migrations/2024_10_16_075112_create_manufacturers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('manufacturers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('country');
+            $table->date('fouded_year');
+            $table->string('website');
+            $table->string('logo');
+            $table->text('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('manufacturers');
+    }
+};
