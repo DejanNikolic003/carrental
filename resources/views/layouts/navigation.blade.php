aa8e9d7ba050a7c74d88ef2e9847d6cde920c8cc
DZSONY <knolekjhnny@gmail.com>
2024-10-17 10:26:41 +0200

    Added "manufacturer" route.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('manufacturers.index')" :active="request()->routeIs('manufacturers.index')">
+                        {{ __('Manufacturers') }}
+                    </x-nav-link>
                 </div>
             </div>
 
